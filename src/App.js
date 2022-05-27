4051fe166304bef28304616e1f73b4ad259ab58d
Sudaraka Gamage <ashenchamli220@gmail.com>
2022-05-27 14:40:34 +0530

    Top Bar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Home from "./pages/home/Home";
+import TopBar from "./topbar/TopBar";
 
 
 function App() {
   return (
-    <div className="App">
-      Blog app
-    </div>
+    <>
+    <TopBar/>
+    <Home/>
+    </>
   );
 }
 
