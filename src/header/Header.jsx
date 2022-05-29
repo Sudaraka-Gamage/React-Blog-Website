2448dc9adc6b7888ba9fdbab6dbe3ec3bbe0b60c
Sudaraka Gamage <ashenchamli220@gmail.com>
2022-05-29 13:30:18 +0530

    Header Component

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,6 +2,14 @@ import "./header.css"
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleSm">React & Node</span>
+            <span className="headerTitleLg">Blog</span>
+        </div>
+        <img className="headerImg" 
+             src="https://images.pexels.com/photos/1766838/pexels-photo-1766838.jpeg?cs=srgb&dl=pexels-baskin-creative-studios-1766838.jpg&fm=jpg" 
+             alt="" />
+    </div>
   )
 }
